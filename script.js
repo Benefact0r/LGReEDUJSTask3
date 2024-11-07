1bbdfe509589c6d53500c5d2566daad77d3ca5e0
lukagvenetadze <lukagvenetadze04@gmail.com>
2024-11-07 12:29:35 +0400

    commit2

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-let age = 18;
-let ticketprice;
+let personAge = 0;
+let ticketPrice;
 
-if ( age <= 12) {
-    console.log('tkt price is 5$');
-    console.log('under 12');
+
+if ( personAge >= 1 && personAge <= 12) {
+    ticketPrice = 5;
+    console.log('Ticket price is ', ticketPrice, '$');
 } 
-else if ( age > 12 && age <= 17) {
-    console.log('tkt price is 10$');
-    console.log('age is 12-17');
-} 
-else if (age >= 18 && age <= 64) {
-    console.log('tkt price is 15$');
-    console.log('age is 18-64');
-} 
-else if ( age >= 65 ) {
-    console.log('tkt price is 8$');
-    console.log('age is 65-120');
+else if ( personAge > 12 && personAge <= 17) {
+    ticketPrice = 10;
+    console.log('Ticket price is ', ticketPrice, '$');
 }
+else if ( personAge >= 18 && personAge <=64) {
+    ticketPrice = 15;
+    console.log('Ticket price is ', ticketPrice, '$');
+}
+else if ( personAge >= 65 && personAge <= 120 ) {
+    ticketPrice = 8
+    console.log('Ticket price is ', ticketPrice, '$');
+}
+else if ( personAge < 0 || personAge > 120) {
+    console.log('Please try again');
+}
+else {console.log('Please check your informations');}
