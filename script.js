67ccab5e82892fa0b9050c5daef781da584e9e3f
lukagvenetadze <lukagvenetadze04@gmail.com>
2024-11-07 14:50:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ let ticketPrice;
 
 if ( personAge >= 1 && personAge <= 12) {
     ticketPrice = 5;
-    console.log('Ticket price is ', ticketPrice, '$');
-} 
-else if ( personAge > 12 && personAge <= 17) {
+    console.log(`Ticket price is ${ticketPrice}$`);
+
+} else if ( personAge > 12 && personAge <= 17) {
     ticketPrice = 10;
-    console.log('Ticket price is ', ticketPrice, '$');
-}
-else if ( personAge >= 18 && personAge <=64) {
+    console.log(`Ticket price is ${ticketPrice}$`)
+
+} else if ( personAge >= 18 && personAge <=64) {
     ticketPrice = 15;
-    console.log('Ticket price is ', ticketPrice, '$');
-}
-else if ( personAge >= 65 && personAge <= 120 ) {
+    console.log(`Ticket price is ${ticketPrice}$`)
+
+} else if ( personAge >= 65 && personAge <= 120 ) {
     ticketPrice = 8
-    console.log('Ticket price is ', ticketPrice, '$');
-}
-else if ( personAge < 0 || personAge > 120) {
+    console.log(`Ticket price is ${ticketPrice}$`)
+
+} else if ( personAge < 0 || personAge > 120) {
     console.log('Please try again');
+
+} else {
+    console.log('Please check your informations');
 }
-else {console.log('Please check your informations');}
